2a64c35850558d7e281cba6a54b9fd119780dd51
jordandickow <jdickow96@gmail.com>
2019-10-06 16:06:51 -0400

    added:  - send button  - test message  - add textfield  - contact list (array of contacts)

--- genysys-chat/src/Papersheet.js
+++ genysys-chat/src/Papersheet.js
@@ -2,6 +2,12 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Chip from '@material-ui/core/Chip';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -13,24 +19,28 @@ flex: {
 
 
 
+
 },
-topicWindow: {
-    width: '30%',
+contactWindow: {
+    width: '25%',
     height: '300px',
-    borderRight: '1px solid grey'
+    borderRight: '1px solid black'
 
 },
 
 chatWindow: {
-    width: '70%'
+    width: '100%',
+    padding: '20px'
 },
 
 chatBox: {
-    width: '85%'
+    width: '100%'
+
 },
 
 button: {
-    width: '15%'
+    width: '5%',
+    marginTop: '310px'
 
 }
 
@@ -47,11 +57,48 @@ export default  function Papersheet() {
                 Chat App
                 </Typography>
                 <Typography variant="h5" component="h5">
-                Topic
+                Contacts
                 </Typography>
                 <div className={classes.flex}>
-                    <div className={classes.topicWindow}>
+                    <div className={classes.contactWindow}>
+                    <List>
+                    {
+                        ['contact'].map(contact => (
+                            <ListItem key={contact} button>
+                                <ListItemText primary={contact} />
+                            </ListItem>
+
+                        ))
+                    }
+
+                    </List>
                     </div>
+                    <div className={classes.chatWindow}>
+                    {
+                        [{from: 'user', msg: 'hello'}].map((chat,i) => (
+                            <div className={classes.flex} key={i}>
+                                 <Chip label={chat.from} className={classes.chip} />
+                                 <Typography variant='p'>{chat.msg}</Typography>
+                            </div>
+
+                        ))
+                    }
+                    </div>
+                        <div className={classes.chatBox}>
+                        <TextField
+                        id="standard-name"
+                        label="Send a chat"
+                        className={classes.chatBox}
+                        // value={values.name}
+                        // onChange={handleChange('name')}
+
+                        />
+                        </div>
+                            <div className={classes.Button}>
+                                <Button variant="contained" color="primary" className={classes.button}>
+                                    Send
+                                </Button>
+                            </div>
 
                 </div>
      </Paper>
